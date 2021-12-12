3d3ccb25081251309d2caa2dcd7c0a19728bd5f0
Basil Benooy <96016648+basilbenooy@users.noreply.github.com>
2021-12-12 16:43:54 +0000

    Update CoronavirusTrackerApplication.java

--- spring covid-19/src/main/java/io/alanbinu/coronavirustracker/CoronavirusTrackerApplication.java
+++ spring covid-19/src/main/java/io/alanbinu/coronavirustracker/CoronavirusTrackerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CoronavirusTrackerApplication {
 
 	public static void main(String[] args) {
+	//application startes
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 	}
 
