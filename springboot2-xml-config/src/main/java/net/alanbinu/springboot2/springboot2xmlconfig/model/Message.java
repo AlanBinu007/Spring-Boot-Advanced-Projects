781439ebf3c6c3bb4d1b7432d1e247e257f48076
Gyathri Ravi <95992675+Gyathriravi@users.noreply.github.com>
2021-12-12 04:12:27 +0000

    Update Message.java

--- springboot2-xml-config/src/main/java/net/alanbinu/springboot2/springboot2xmlconfig/model/Message.java
+++ springboot2-xml-config/src/main/java/net/alanbinu/springboot2/springboot2xmlconfig/model/Message.java
@@ -1,7 +1,9 @@
 package net.alanbinu.springboot2.springboot2xmlconfig.model;
 
 public class Message {
+	//to store the uniqe id
 	private int id;
+	// to store the string message
 	private String message;
 	public Message(int id, String message) {
 		super();
