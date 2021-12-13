0f46175caf7b3bba4e9308111e55d317770d0648
Shibu Shibu <96076443+Shibushibu1@users.noreply.github.com>
2021-12-13 23:18:10 +0530

    Update ResourceNotFoundException.java

--- springboot-jpa-one-to-one-example/src/main/java/net/alanbinu/springboot/jpa/controller/ResourceNotFoundException.java
+++ springboot-jpa-one-to-one-example/src/main/java/net/alanbinu/springboot/jpa/controller/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package net.guides.springboot.jpa.controller;
+package net.alanbinu.springboot.jpa.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
