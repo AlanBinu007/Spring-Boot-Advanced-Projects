b5e9017c9732d9b4b98119227c62d7891df086b0
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-12-16 09:38:08 +0530

    Spring boot Data REST

---
+++ spring-boot-2-jpa-spring-data-rest/src/test/java/com/alanbinu/springboot/jpa/spring/data/rest/example/SpringBoot2JPAWithSpringDataRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alanbinu.springboot.jpa.spring.data.rest.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2JPAWithSpringDataRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
