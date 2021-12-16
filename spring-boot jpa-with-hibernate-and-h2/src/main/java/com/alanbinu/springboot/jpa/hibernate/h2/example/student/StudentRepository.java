28877390e2581d549625c9bb37810f7949d5c3a5
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-12-16 09:43:27 +0530

    Spring boot hibernate with H2

---
+++ spring-boot jpa-with-hibernate-and-h2/src/main/java/com/alanbinu/springboot/jpa/hibernate/h2/example/student/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.alanbinu.springboot.jpa.hibernate.h2.example.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
