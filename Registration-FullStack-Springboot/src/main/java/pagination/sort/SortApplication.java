974a1aacc3e72c8088b2d7539a2de734fd0cd72e
Alan Binu <alanbinu5@gmail.com>
2021-12-16 14:48:22 +0530

    Registration-FullStack-Springboot

---
+++ Registration-FullStack-Springboot/src/main/java/pagination/sort/SortApplication.java
@@ -0,0 +1,13 @@
+package pagination.sort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortApplication.class, args);
+    }
+
+}
---
+++ Registration-FullStack-Springboot/src/test/java/pagination/sort/SortApplicationTests.java
@@ -0,0 +1,13 @@
+package pagination.sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
