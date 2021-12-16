e250eb5e909cf32f29bed05025dcbe7be2163346
Alan Binu <alanbinu5@gmail.com>
2021-12-16 17:02:07 +0530

    SpringBoot-Swagger2-Enabling

---
+++ SpringBoot-Swagger2-Enabling/src/main/java/spring/swagger/SwaggerAPIController.java
@@ -0,0 +1,29 @@
+package spring.swagger;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class SwaggerAPIController {
+    //
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public List<String> getProducts() {
+        List<String> productsList = new ArrayList<>();
+        productsList.add("Honey");
+        productsList.add("Almond");
+        return productsList;
+    }
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public String createProduct() {
+        return "Product is saved successfully";
+    }
+//    @RequestMapping(value = "/products", method = RequestMethod.PUT)
+//    public String updateProduct(){
+//        return "Product is updated";
+//    }
+}
